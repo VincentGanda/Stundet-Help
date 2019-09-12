9bf215bfa41c5c49140cbc082f72209c0ca8b599
Vincent <vincentganda@gmail.com>
2019-09-12 18:59:20 +0700

    nambah footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavbarComp from './components/Navbar/Navbar'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import Auth from './components/Auth/Auth'
 import Background from './components/Background/Background'
+import FooterPage from './components/Footer/Footer'
 
 
 class App extends Component{
@@ -15,6 +16,7 @@ class App extends Component{
         {/* <Route component={Homepage} path='/' exact /> */}
         <Route component={Auth} path='/auth' exact/>
       </Switch>
+      <FooterPage />
       </div>
     )
   }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+
+const FooterPage = () => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-2 mt-2">
+      <MDBContainer fluid className="text-center text-md-left">
+        <MDBRow>
+          <MDBCol md="6">
+            <h5 className="title">Got any questions? Want to do collaboration?</h5>
+            <p>
+              contact me via e-mail at vincentganda625@gmail.com
+            </p>
+          </MDBCol>
+      
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href=""> Vincent Ganda </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default FooterPage;
